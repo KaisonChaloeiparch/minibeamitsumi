ec9e0baf94e1d46e08fcafaa8d97b2ffd75f94cf
KaisonChaloeiparch <151432957+KaisonChaloeiparch@users.noreply.github.com>
2025-01-26 15:02:55 +0700

    Minebea

---
+++ database/products.sql
@@ -0,0 +1,4 @@
+INSERT INTO products (nameparttool, price, descriptionpartname, image)
+VALUES 
+('nameparttool1', 99.99, 'Descriptionpartname for nameparttool1', 'https://picsum.photos/201'),
+('nameparttool2', 149.50, 'Descriptionpartname for nameparttool2 ', 'https://picsum.photos/202');
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,13 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Models\Product;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -22,4 +24,26 @@ require __DIR__.'/auth.php';
 Route::get('/', function () {
     // return view('welcome');
     return view('home');
+});
+
+
+// use App\Models\Product;
+// use Illuminate\Support\Facades\DB;
+
+Route::get('query/sql', function () {
+    $products = DB::select("SELECT * FROM products");
+    // $products = DB::select("SELECT * FROM products WHERE price > 100");
+    return view('query-test', compact('products'));
+});
+
+Route::get('query/builder', function () {
+    $products = DB::table('products')->get();
+    // $products = DB::table('products')->where('price', '>', 100)->get();
+    return view('query-test', compact('products'));
+});
+
+Route::get('query/orm', function () {
+    $products = Product::get();
+    // $products = Product::where('price', '>', 100)->get();
+    return view('query-test', compact('products'));
 });
